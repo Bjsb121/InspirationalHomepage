d969bd73f4c54b34d321473711f70466c77193a6
Bryan Schmidt <bryanschmidt@MacBookAir.attlocal.net>
2025-01-13 22:45:14 -0600

    Added title and working on text box

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
 import './App.css';
+import React, { useState } from 'react';
 
-function App() {
-  return;
+function TextBox() {
+  const [value, setValue] = useState('');
+
+  const handleChange = (e) => {
+    setValue(e.target.value);
+  };
+
+  return (
+    <div>
+      <label htmlFor="text-box" className="title">Things to do today: </label>
+      <input
+        id="text-box"
+        type="text"
+        value={value}
+        onChange={handleChange}
+        placeholder="What are we doing today?"
+      />
+      <div className='itemBoxBox'>
+        <div className='itemBox'>
+          <p className={`items ${!value ? 'blinking' : ''}`}>{value}</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default App;
+export default TextBox;
